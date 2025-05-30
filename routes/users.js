885ec2618d57f1786e640d5ff0faf0ce5c121d35
haakoaal <haakoaal@stud.ntnu.no>
2025-05-30 11:40:18 +1000

    fix: profile updates format validation

--- routes/users.js
+++ routes/users.js
@@ -164,7 +164,7 @@ router.post('/refresh', async (req, res, next) => {
     if (error.name === 'TokenExpiredError') {
       return res.status(401).json({error: true, message: 'JWT token has expired'});
     } else {
-      return res.status(500).json({error: true, message: `Refresh Error: ${error.message}`});
+      return res.status(401).json({error: true, message: 'Invalid JWT token'});
     }
   }
 });
@@ -200,7 +200,7 @@ router.post('/logout', async (req, res, next) => {
     if (error.name === 'TokenExpiredError') {
       return res.status(401).json({error: true, message: 'JWT token has expired'});
     } else {
-      return res.status(500).json({error: true, message: `Refresh Error: ${error}`});
+      return res.status(401).json({error: true, message: 'Invalid JWT token'});
     }
   }
 });
@@ -299,6 +299,35 @@ router.put("/:email/profile", authMiddleware(true), async (req, res, next) => {
 
     if (decodedJWT.email === providedEmail) {
       // Authorized for update
+      // Checking parameters:
+      const stringProperties = ["firstName", "lastName", "address"];
+      const stringCheck = stringProperties.every(p => typeof profileUpdates[p] === 'string');
+
+      if (!stringCheck) return res.status(400).json({error: true,message: "Request body invalid: firstName, lastName and address must be strings only."});
+      // Check dob property format
+      // \d === [0-9]
+      if (typeof profileUpdates.dob != 'string'  ||  !/^\d\d\d\d-\d\d-\d\d$/.test(profileUpdates.dob)){
+        return res.status(400).json({error: true,message: "Invalid input: dob must be a real date in format YYYY-MM-DD."});;
+      }
+      
+      const generatedDate = new Date(profileUpdates.dob);
+      const splitDate = profileUpdates.dob.split('-');
+      const year = splitDate[0]
+      const month = splitDate[1]
+      const day = splitDate[2]
+
+      if (generatedDate.getFullYear() != year ||
+        generatedDate.getMonth() != month - 1 ||
+        generatedDate.getDate() != day
+      ){
+        return res.status(400).json({error: true,message: "Invalid input: dob must be a real date in format YYYY-MM-DD."});;
+      } 
+      
+      const today = new Date();
+      if (generatedDate > today) {
+        return res.status(400).json({ error: true, message: "Invalid input: dob must be a date in the past." });
+      }
+
       await profileBaseQuery.update(profileUpdates);
 
       const updatedUser = await req.db('users')
